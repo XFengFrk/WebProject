e37b48440e24ef342f752bb82db8edc7946130dd
Nepsyn <747337480@qq.com>
2020-06-28 23:45:07 +0800

    * 修复了一个关键bug

--- src/main/java/org/ep2/hrmsdt/interceptor/LoginInterceptor.java
+++ src/main/java/org/ep2/hrmsdt/interceptor/LoginInterceptor.java
@@ -12,6 +12,6 @@ public class LoginInterceptor implements HandlerInterceptor {
             response.sendRedirect(request.getContextPath() + "/index.html");
         }
 
-        return false;
+        return true;
     }
 }
