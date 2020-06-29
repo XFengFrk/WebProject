7454c465086f544b3558436d2d2bcda7a2208f6a
Nepsyn <747337480@qq.com>
2020-06-29 12:26:50 +0800

    * 修复了跨域请求的bug

--- src/main/java/org/ep2/hrmsdt/interceptor/LoginInterceptor.java
+++ src/main/java/org/ep2/hrmsdt/interceptor/LoginInterceptor.java
@@ -8,8 +8,9 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if (request.getSession().getAttribute("username") == null) {
-            response.sendRedirect(request.getContextPath() + "/index.html");
+        if (request.getSession().getAttribute("username") == null && !request.getMethod().equals("OPTIONS")) {
+            response.sendError(response.SC_FORBIDDEN, "login required!");
+            return false;
         }
 
         return true;
